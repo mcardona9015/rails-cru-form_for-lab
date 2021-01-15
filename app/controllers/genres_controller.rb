8ebcf1669520c1e2782db776f5eaf01523f7210d
mcardona9015 <73857382+mcardona9015@users.noreply.github.com>
2021-01-15 10:41:49 -0500

    first

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,10 @@
+class GenresController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  get '/artists/', to: 'artists#index', as: 'artists'
+
+  get '/artists/new', to: 'artists#new', as: 'new_artist'
+
+  post '/artists/', to: 'artists#create'
+
+  get '/artists/:id', to: 'artists#show', as: 'artist'
+
+  get '/artists/:id/edit', to: 'artists#edit', as: 'edit_artist'
+
+  patch '/artists/:id', to: 'artists#update'
+
+  get '/songs/',to: 'songs#index', as: 'songs'
+
+  get '/songs/new', to: 'songs#new', as: 'new_song'
+
+  post '/songs/', to: 'songs#create'
+
+  get '/songs/:id', to: 'songs#show', as: 'song'
+
+  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+
+  patch '/songs/:id', to: 'songs#update'
+
+  get '/genres/', to: 'genres#index', as: 'genres'
+
+  get '/genres/new', to: 'genres#new', as: 'new_genre'
+
+  post '/genres/', to: 'genres#create'
+
+  get '/genres/:id', to: 'genres#show', as: 'genre'
+
+  get '/genres/:id/edit', to: 'genres#edit', as: 'edit_genre'
+
+  patch '/genres/:id', to: 'genres#update'
+
 end
